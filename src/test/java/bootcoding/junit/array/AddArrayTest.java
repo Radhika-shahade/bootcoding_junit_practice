8eef6fda8963bbd039ba89c66b1b8288bdfa6797
Radhika shahade <radhikashahade0804@gmail.com>
2023-09-12 13:46:14 +0530

    junit pr

--- src/test/java/bootcoding/junit/array/AddArrayTest.java
+++ src/test/java/bootcoding/junit/array/AddArrayTest.java
@@ -6,19 +6,34 @@ import static org.junit.Assert.*;
 
 public class AddArrayTest {
     @Test
-    public void test()
+    public void testWithPositiveNumbers()
     {
         AddArray obj= new AddArray();
         int arr[]={3,4,5,1,2};
-        int expected= obj.addArray(arr);
-        int actualSum= 15;
+        int actualSum = obj.addArray(arr);
+        int expected= 15;
         assertEquals(expected,actualSum);
     }
     @Test
-    public void test2()
-    {
+    public void testWithNegativeNumbers() {
+        int arr[] = {-1, -3, -4, -5};
+        int expectedSum = -13;
+        AddArray addArray = new AddArray();
+        int actualSum = addArray.addArray(arr);
+        assertEquals(expectedSum, actualSum);
+    }
+
+     @Test
+       public void testWithMixedNumbers()
+        {
+            int arr[]={1 ,-2 ,-3 ,-4 ,5};
+             int expectedSum= -3;
+             AddArray addArray= new AddArray();
+           int actualSum=addArray.addArray(arr);
+             assertEquals(expectedSum,actualSum);
+        }
+
 
     }
 
 
-}
