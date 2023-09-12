e43f364d548f84e635794858723ae63d86333599
Radhika shahade <radhikashahade0804@gmail.com>
2023-09-12 13:47:08 +0530

    junit examples

--- src/test/java/bootcoding/junit/array/FindMaxTest.java
+++ src/test/java/bootcoding/junit/array/FindMaxTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 public class FindMaxTest {
     @Test
-    public void test()
+    public void testWithPositiveNumbers()
     {
         FindMax max= new FindMax();
         int arr[]={3,4,5,1,2};
@@ -14,6 +14,34 @@ public class FindMaxTest {
         int expected= 5;
         assertEquals(expected,actual);
     }
+    @Test
+    public void testWitheNumbers(){
+        int arr[]={2,33,45,67};
+        int expectedAns=67;
+        FindMax max= new FindMax();
+        int actualAns= max.findMaxElement(arr);
+        assertEquals(expectedAns,actualAns);
+    }
+    @Test
+    public void testWithNegativeNumbers()
+    {
+        int arr[]={-3,-4,-55,-9};
+        int expectedAns=-3;
+        FindMax max= new FindMax();
+        int actualAns=max.findMaxElement(arr);
+        assertEquals(expectedAns,actualAns);
+    }
+    @Test
+    public void testWithZeroAndNegativeNum()
+    {
+        int arr[]={0,-1,-3,-6};
+        int expectedAns=0;
+        FindMax max= new FindMax();
+        int actualAns=max.findMaxElement(arr);
+        assertEquals(expectedAns,actualAns);
+    }
+
+
 
 
 }
